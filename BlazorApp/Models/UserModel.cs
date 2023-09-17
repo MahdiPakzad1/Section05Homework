b2b6c04aa97ba72c06933226b503054cd08da4b1
Mahdi Pakzad <elias-developer@outlook.com>
2023-09-17 17:46:36 +0330

    Add project files.

---
+++ BlazorApp/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Models;
+
+public class UserModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Message { get; set; }
+
+
+}
